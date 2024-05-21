0487a2bbb8d50ae48e04cd9358cbb44a09e2fecc
Саида <129662788+Saida3232@users.noreply.github.com>
2024-05-22 00:04:58 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,8 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DJ_DEBUG', 'False').lower() == 'true'
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS = ['158.160.15.239', '127.0.0.1', 'localhost', 'recipegram.zapto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 INSTALLED_APPS = [
